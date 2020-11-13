2448828526e649b0814bca31b0284f974d2d8012
NikitaSparkle <71117913+nikitasparkle@users.noreply.github.com>
2020-11-13 19:28:16 +0200

    1 - 4 finished stopped on 5

--- src/main/java/homework/StringCalc.java
+++ src/main/java/homework/StringCalc.java
@@ -1,14 +1,20 @@
 package homework;
 
 public class StringCalc {
-    public static int add(String s){
-        if(s==null || "".equals(s)) {
+    public static int add(String s) {
+        if (s == null || "".equals(s)) {
             return 0;
         }
+        s = s.replaceAll("\n", ",");
+        s = s.replaceAll("//", ",");
+        s = s.replaceAll(";", ",");
+        s = s.replaceAll("#", ",");
         String[] nums = s.split(",");
-        int sum=0;
-        for(String num:nums){
-            sum+=Integer.parseInt(num.trim());
+        int sum = 0;
+        for (String num : nums) {
+            if (Integer.parseInt(num.trim()) <= 1000) {
+                sum += Integer.parseInt(num.trim());
+            }
         }
         return sum;
     }
--- src/main/java/homework/StringCalcTest.java
+++ src/main/java/homework/StringCalcTest.java
@@ -71,10 +71,92 @@ public class StringCalcTest {
         }
     }
 
+    static void kata03(){
+        System.out.println(" kata 03");
+        int error=0;
+
+        if(StringCalc.add("1\n2,3") != 6){
+            error+=1;
+            System.err.println("Ошибка строки: 1\n2,3");
+        }
+
+        if(StringCalc.add("4\n5\n6") != 15){
+            error+=1;
+            System.err.println("Ошибка строки: 4\n5\n6");
+        }
+
+        if(StringCalc.add("77\n") != 77){
+            error+=1;
+            System.err.println("Ошибка строки: 77\n");
+        }
+        else{
+            System.out.println("Successful");
+        }
+    }
+
+    static void kata04(){
+        System.out.println(" kata 04");
+        int error=0;
+
+        if(StringCalc.add("1,1001") != 1){
+            error+=1;
+            System.err.println("Ошибка строки: 1,1001");
+        }
+
+        if(StringCalc.add("1002\n2") != 2){
+            error+=1;
+            System.err.println("Ошибка строки: 1002\n2");
+        }
+
+        if(StringCalc.add("1,1000") != 1001){
+            error+=1;
+            System.err.println("Ошибка строки: 1,1000");
+        }
+        if(StringCalc.add("1000,1000") != 2000){
+            error+=1;
+            System.err.println("Ошибка строки: 1000,1000");
+        }
+        if(StringCalc.add("1001\n1001") != 0){
+            error+=1;
+            System.err.println("Ошибка строки: 1001\n1001");
+        }
+        else{
+            System.out.println("Successful");
+        }
+    }
+
+    static void kata05(){
+        System.out.println(" kata 05");
+        int error=0;
+
+        if(StringCalc.add("//;\n1;2") != 3){
+            error+=1;
+            System.err.println("Ошибка строки: //;\n1;2");
+        }
+
+        if(StringCalc.add("//\n2,3") != 5){
+            error+=1;
+            System.err.println("Ошибка строки: //*\n2,3");
+        }
+
+        if(StringCalc.add("//#\n3#4") != 7){
+            error+=1;
+            System.err.println("Ошибка строки: //#\n3#4");
+        }
+        else{
+            System.out.println("Successful");
+        }
+    }
 
     public static void main(String[] args) {
         kata01();
         System.out.println("\n");
         kata02();
+        System.out.println("\n");
+        kata03();
+        System.out.println("\n");
+        kata04();
+        System.out.println("\n");
+        kata05();
     }
 }
